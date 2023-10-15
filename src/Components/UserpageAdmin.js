d0647948a917e4d623fbea872797a1b48e60bee4
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2023-10-15 12:04:43 +0530

    Right side of users page.
    Right side of user page is for admin, it cannot be accessed by the user.

---
+++ src/Components/UserpageAdmin.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, IconButton } from "@mui/material";
+import LockPersonIcon from "@mui/icons-material/LockPerson";
+import { useNavigate } from "react-router-dom";
+import "./UserpageAdmin.css";
+
+export default function UserpageAdmin() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>
+        <div className="Heading">
+          <h2 style={{ paddingLeft: "45%" }}>ADMIN</h2>
+        </div>
+
+        <Box className="AdminBox">
+          <IconButton onClick={() => navigate("/adminlogin")}>
+            <LockPersonIcon className="Adminicon" />
+          </IconButton>
+        </Box>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -1,5 +1,36 @@
 import React from "react";
+import UserpageAdmin from "../Components/UserpageAdmin";
+import UserpageUser from "../Components/UserpageUser";
+import { Grid } from "@mui/material";
+import "./Users.css";
 
 export default function Users() {
-  return <div>Users</div>;
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2
+          style={{
+            fontSize: 30,
+          }}
+        >
+          Welcome to Timetable management system
+        </h2>
+      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <UserpageUser />
+        </Grid>
+        <div className="Line"></div>
+        <Grid item xs={6}>
+          <UserpageAdmin />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
